f2d792ceed843d77050444e72a16675f02ed35b1
godfreym1978 <godfreym@gmail.com>
2021-10-16 15:03:46 -0500

    Update ConnectDB.java

--- src/com/Godfrey/Finance/ConnectDB.java
+++ src/com/Godfrey/Finance/ConnectDB.java
@@ -6,19 +6,19 @@ import java.sql.SQLException;
 
 public class ConnectDB {
 
-	/* For Oracle */
+	/* For Oracle 
 	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	private static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost:1521:XE";
 	private static final String DB_USER = "system";
 	private static final String DB_PASSWORD = "passw9rd";
-	
+	*/
 
-	/* For mysql 
+	/* For mysql */
 	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/finance?useSSL=false";
 	private static final String DB_USER = "root";
-	private static final String DB_PASSWORD = "passw3rd";
-	*/
+	private static final String DB_PASSWORD = "passw0rd";
+	
 	
 	public Connection getDBConnection() {
 
